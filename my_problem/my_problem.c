5f1e1217d482495f3e2e04b666d550cc3818c577
wojiaoxiaofuaaa <2359303334@qq.com>
2023-12-13 02:16:58 +0800

    2023/12/13 首次使用 添加了一些相关的常用的代码

---
+++ my_problem/my_problem.c
@@ -0,0 +1,6 @@
+/*
+error setting certificate verify locations: CAfile:问题  出现相关问题 
+问题出现的原因：  git存放位置出现问题  
+how to slove this problem  :  git config --system http.sslverify false  
+it can slove  this
+*/
